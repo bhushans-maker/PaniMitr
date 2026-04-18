e0a3fd48305fb131d4490005dd4232f005d96579
bhushans-maker <bhushan.s@carwale.com>
2026-04-19 01:24:08 +0530

    Delete aquaalarm (2) directory

--- aquaalarm (2)/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.aquaalarm.app',
-  appName: 'AquaAlarm',
-  webDir: 'dist'
-};
-
-export default config;
